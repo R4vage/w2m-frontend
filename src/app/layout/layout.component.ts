a2f616b48d74f48709ad978062d0dae759dec838
R4vage <jmarc@applaudostudios.com>
2023-10-28 13:33:49 -0300

    Finished main layout.

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent {
+  menuItems = Object.values(this.router.routesEnum);
+  constructor(private router: Router) {
+    console.log(this.menuItems)
+  }
 
+  navigateToUrl(url:string) {}
 }
