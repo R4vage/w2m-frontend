cd237c34f9d3f16cec5acd11a2c3321a970ecc38
R4vage <jmarc@applaudostudios.com>
2023-10-28 13:35:03 -0300

    Included lazy loading. Setup layout.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppRoutes } from './core/router/routes.enum';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: AppRoutes.SUPERHEROES,
+    loadChildren: () =>
+      import('./superheroes/superheroes.module').then(
+        (m) => m.SuperheroesModule
+      ),
+  },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./error-page/error-page.module').then((m) => m.ErrorPageModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ExtendedRouter } from './core/router/extended-router.service';
+import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, LayoutModule],
+  providers: [{ provide: Router, useClass: ExtendedRouter }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
