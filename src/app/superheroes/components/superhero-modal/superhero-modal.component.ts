dc2f3e841dc384fa459811d696e84cc18de04894
R4vage <jmarc@applaudostudios.com>
2023-10-29 12:41:33 -0300

    Finished form modal

--- src/app/superheroes/components/superhero-modal/superhero-modal.component.ts
+++ src/app/superheroes/components/superhero-modal/superhero-modal.component.ts
@@ -1,7 +1,7 @@
 // superhero-modal.component.ts
 import { Component, Inject, OnInit, Optional } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Superhero } from '../../store/superhero.model'; 
 
 @Component({
@@ -14,13 +14,14 @@ export class SuperheroModalComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
-    @Optional() @Inject(MAT_DIALOG_DATA) public data: Superhero
+    @Optional() @Inject(MAT_DIALOG_DATA) public data: Superhero,
+    private dialogRef: MatDialogRef<SuperheroModalComponent>
   ) {}
 
   ngOnInit(): void {
     this.superheroForm = this.fb.group({
       id: [
-        this.data?.id || '',
+        {value: this.data?.id || '', disabled: !!this.data},
         [
           Validators.required,
           Validators.minLength(4),
@@ -61,4 +62,11 @@ export class SuperheroModalComponent implements OnInit {
       ],
     });
   }
+
+  onSubmit() {
+    if (this.superheroForm.valid) {
+      this.dialogRef.close(this.superheroForm.value);
+    }
+  }
+
 }
